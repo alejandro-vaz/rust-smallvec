670dfc7f1ec3c40038656577c4a5dc5219757dd0
Alejandro Vaz <alejandro.vaz.myt@gmail.com>
2026-08-29 14:51:38 +0200

    refactor: renamed generic

--- src/borsh.rs
+++ src/borsh.rs
@@ -1,18 +1,15 @@
 use super::SmallVec;
 use borsh::{
+    io::{Result as Serial, Write},
     BorshSerialize,
-    io::{
-        Write,
-        Result as Serial
-    }
 };
 
-impl<Type: BorshSerialize, const N: usize> BorshSerialize for SmallVec<Type, N> {
+impl<Type: BorshSerialize, const CAPACITY: usize> BorshSerialize for SmallVec<Type, CAPACITY> {
     fn serialize<Writer: Write>(&self, writer: &mut Writer) -> Serial<()> {
         self.len.0.serialize(writer)?;
         for element in self {
             element.serialize(writer)?;
-        };
+        }
         return Ok(());
     }
 }
